eedc06bbdfde3ec00df45df1fb324911570049be
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2023-12-25 20:07:33 +0000

    primitives number

---
+++ Basics/Primitives.java
@@ -0,0 +1,19 @@
+public class Primitives {
+    public static void main(String[] args){
+        int roll_no = 663;
+        char letter = 'r';
+        //Single quotes for char, double for strings.
+
+        float marks = 98.5f;
+        double position = 54325456.789746;
+        //double is used for large decimal numbers.
+
+        long marks2 = 7548395456437564L;
+        //long is used for large integer numbers.
+
+        boolean check = false;
+        //String is not a primitive datatype. Primitive means that cannot be broken even further.
+
+
+    }
+}
