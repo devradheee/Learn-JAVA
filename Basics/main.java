085795d0013a17996222cd64522c704ae1cc1d10
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2023-12-25 20:17:19 +0000

    main class

---
+++ Basics/main.java
@@ -0,0 +1,13 @@
+package com.First;
+
+import java.util.Scanner;
+
+public class main {
+     {
+    public static void main(String[] args) {
+        System.out.print("Hello World! ");
+        System.out.println("My name is Radheshyam kushwaha.");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
