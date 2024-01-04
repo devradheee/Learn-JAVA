ac2419629dac2c57d26f47d9e4c47bb4bbb412e7
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-01-04 18:49:03 +0000

    integerLiteralquestion

---
+++ 01_LITERAL/IntegerLiteralQues.java
@@ -0,0 +1,12 @@
+public class IntegerLiteralQues {
+        static public void main(String[] s) {
+            int a = 05;
+            // int b=08; //error
+            // int b=09; //error
+            int b = 010;
+            System.out.println(a);
+            System.out.println(b);
+        }
+    }
+    
+
