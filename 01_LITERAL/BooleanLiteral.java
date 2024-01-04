fbe8e22ea242049faca5506c5482ae34c23229d0
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-01-04 18:28:29 +0000

    booleanLiteral

---
+++ 01_LITERAL/BooleanLiteral.java
@@ -0,0 +1,11 @@
+class BooleanLiteral{
+    static public void main(String [] s){
+        boolean a=true;//BooleanLiteral
+        boolean b=false;//BooleanLiteral
+        //boolean b="false";//error
+        //boolean b='false';//error
+        //boolean b=False;//error
+        System.out.println(a); 
+        System.out.println(b); 
+    }
+}
