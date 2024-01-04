4c6674f2ac2a10274cd9ac5b3a2918b0962f702f
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-01-04 18:52:12 +0000

    StringLiteral

---
+++ 01_LITERAL/StringLiteral.java
@@ -0,0 +1,18 @@
+public class StringLiteral {
+        static public void main(String[] s) {
+            String a = "Devradheee git"; // String Literal
+            String b = "104"; // String Literal
+            String c = "2.6"; // String Literal
+            String d = "true"; // String Literal
+            String e = "h"; // String Literal
+            System.out.println(a);// Devradheee git
+            System.out.println(b);// 104
+            System.out.println(c);// 2.6
+            System.out.println(d);// true
+            System.out.println(e);// true
+
+            String x = "_Ra_dhe sh_yam_";// allowed
+            System.out.println(x);// _Ra_dhe sh_yam_
+        }
+    }
+
