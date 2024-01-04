fd5ca4c2268fb5dc2119d4150c20f108329d5f04
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-01-04 19:14:32 +0000

    ArithmeticOperator

---
+++ 03_Operator/ArithmeticOperator.java
@@ -0,0 +1,14 @@
+// package 03_Operator;
+
+public class ArithmeticOperator {
+        public static void main(String[] args) {
+            int a = 7 / 2;
+            System.out.println(a);
+
+            double b = 7 / 2;
+            System.out.println(b);
+
+            double c = 7.0 / 2;
+            System.out.println(c);
+        }
+    }
